ce08123c5dd390d890f255d26cf97b6a78755f69
Waseem Mazari <mazariwaseem110@gmail.com>
2025-03-25 08:50:39 +0500

    Welcome

--- Mid.jsx
+++ Mid.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Mid = () => {
   return (
     <div>
-      <h1>Hello vite</h1>
+      {/* <h1>Hello vite</h1> */}
+      <h2>In the ead mid</h2>
+      <h3>waseem is here</h3>
+      <h2>Welcome </h2>
     </div>
   )
 }
